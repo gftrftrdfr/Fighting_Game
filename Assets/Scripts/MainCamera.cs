bf77edcaa2bbc93b68fb93f6af198ab83cc1721c
Bi <huynhnguyensontral9.1@gmail.com>
2023-01-13 15:56:39 +0700

    Thêm P2, chỉnh Cam

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,21 +1,72 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]  
 public class MainCamera : MonoBehaviour
 {
-    public GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    public List<Transform> players;
+    public Vector3 offset;
+    public float smoothTime = 0.5f;
+    private Vector3 velocity;
+    public float minZoom = 18f;
+    public float maxZoom = 4f;
+    public float zoomLimiter = 60f;
+
+    private void Start()
+    { 
+        players.Add(GameObject.FindGameObjectWithTag("Player 1").transform);
+        players.Add(GameObject.FindWithTag("Player 2").transform);
+    }
+    private void LateUpdate()
     {
+        if(players.Count == 0)
+        {
+            return;
+        }
+        Move();
+        Zoom();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Zoom()
     {
-        Vector3 setPosition = transform.position;
-        setPosition.x = player.transform.position.x;
-        transform.position = setPosition;
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, newZoom, Time.deltaTime);
+
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+        return bounds.size.x;
+    }
+
+    private void Move()
+    {
+        Vector3 center = GetCenterPoint();
+        Vector3 newPos = center + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if(players.Count == 1)
+        {
+            return players[0].position;
+        }
+
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for(int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+        return bounds.center;
     }
 }
